8524f2997907a62eff9392cc1e25e96158c20166
engcang <eungchang_mason@kaist.ac.kr>
2024-03-24 00:47:11 +0900

    typo

--- src/livox_points_plugin.cpp
+++ src/livox_points_plugin.cpp
@@ -42,7 +42,7 @@ void convertDataToRotateInfo(const std::vector<std::vector<double>> &datas, std:
 
 void LivoxPointsPlugin::Load(gazebo::sensors::SensorPtr _parent, sdf::ElementPtr sdf) {
     std::vector<std::vector<double>> datas;
-    std::string file_name = ros::package::getPath("livox_laser_simulation") + sdf->Get<std::string>("csv_file_name");
+    std::string file_name = ros::package::getPath("livox_laser_simulation/") + sdf->Get<std::string>("csv_file_name");
     ROS_INFO_STREAM("load csv file name:" << file_name);
     if (!CsvReader::ReadCsvFile(file_name, datas)) {
         ROS_INFO_STREAM("cannot get csv file!" << file_name << "will return !");
